60fb42c567693d224bda02fb4c330b6683ccb620
Arvind Kumar <arvind@enkrypt.in>
2016-10-15 21:05:27 +0530

    Changed some arguments to work with newer versions of mpv

--- commands/play.js
+++ commands/play.js
@@ -2,7 +2,7 @@ module.exports=function(context){
 	if (context.command!="play"||(context.args[0]||" ").startsWith("http")){
 		return {
 			command: "mpv",
-			args: ["-volume", "50", "--no-video", "--quiet", context.args[0]]
+			args: ["--input-terminal", "-volume=50", "--no-video", "--quiet", context.args[0]]
 		};
 	}
 	else{
